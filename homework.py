fb82b8ac11df0c86b63de1f4bdbdd53aa0aa8b31
m1rageg <egor.sergei.demin153@gmail.com>
2022-11-08 00:19:15 +0200

    upload homework

--- homework.py
+++ homework.py
@@ -1,60 +1,67 @@
 # a
-def biggest_number():
-    print('1) Функция выводит большее число.')
-    first = input('Type 1st number: ')
-    second = input('Type 2nd number: ')
-    if first > second:
-        return first
-    else:
-        return second
+NUMBER_ONE = 2
+NUMBER_TWO = 3
 
 
-biggest_number()
+def biggest_number(number_one, number_two):
+    if number_one > number_two:
+        return number_one
+    if number_two > number_one:
+        return number_two
+    return None
+
+
+biggest_number(NUMBER_ONE, NUMBER_TWO)
 
 
 # b
-def smallest_number():
-    print('2) Функция выводит меньшее число.')
-    first = input('Type 1st number: ')
-    second = input('Type 2nd number: ')
-    third = input('Type 3rd number: ')
+FIRST = 4
+SECOND = 2
+THIRD = 3
+
+
+def smallest_number(first, second, third):
     if first < second and first < third:
         return first
-    elif second < first and second < third:
+    if second < first and second < third:
         return second
-    else:
+    if third < first and third < second:
         return third
+    return None
 
 
-smallest_number()
+smallest_number(FIRST, SECOND, THIRD)
 
 
 # c
-def module_of_number():
-    print('3)Функция выводит модуль числа.')
-    number = input('Type a number: ')
+NUMBER = -3
+
+
+def module_of_number(number):
     return abs(int(number))
 
 
-module_of_number()
+module_of_number(NUMBER)
 
 
 # d
-def sum_of_numbers():
-    print('4)Функция выводит сумму двух чисел.')
-    first = input('Type 1st number: ')
-    second = input('Type 2nd number: ')
-    result = int(first) + int(second)
+FIRST_NUMBER = 2
+SECOND_NUMBER = 4
+
+
+def sum_of_numbers(first_number, second_number):
+    result = first_number + second_number
     print('Sum of your numbers is: ', result)
 
 
-sum_of_numbers()
+sum_of_numbers(FIRST_NUMBER, SECOND_NUMBER)
 
 
 # e
-def is_positive_number():
-    print('5)Функция выводит информацию про знак числа.')
-    number = input('Type a number: ')
+input_number = input('Type a number: ')
+
+
+def is_positive_number(number):
     if int(number) > 0:
         print('Positive')
     elif int(number) == 0:
@@ -63,4 +70,4 @@ def is_positive_number():
         print('Negative')
 
 
-is_positive_number()
+is_positive_number(input_number)
