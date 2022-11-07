a8540ed4eea7878bea93aa77af666197ad581b35
m1rageg <egor.sergei.demin153@gmail.com>
2022-11-07 21:38:09 +0200

    upload homework

--- homework.py
+++ homework.py
@@ -1,12 +1,12 @@
 # a
 def biggest_number():
     print('1) Функция выводит большее число.')
-    a = input('Type 1st number: ')
-    b = input('Type 2nd number: ')
-    if a > b:
-        return a
+    first = input('Type 1st number: ')
+    second = input('Type 2nd number: ')
+    if first > second:
+        return first
     else:
-        return b
+        return second
 
 
 biggest_number()
@@ -15,15 +15,15 @@ biggest_number()
 # b
 def smallest_number():
     print('2) Функция выводит меньшее число.')
-    a = input('Type 1st number: ')
-    b = input('Type 2nd number: ')
-    c = input('Type 3rd number: ')
-    if a < b and a < c:
-        print('The smallest number is: ', a)
-    elif b < a and b < c:
-        print('The smallest number is: ', b)
+    first = input('Type 1st number: ')
+    second = input('Type 2nd number: ')
+    third = input('Type 3rd number: ')
+    if first < second and first < third:
+        return first
+    elif second < first and second < third:
+        return second
     else:
-        print('The smallest number is: ', c)
+        return third
 
 
 smallest_number()
@@ -32,8 +32,8 @@ smallest_number()
 # c
 def module_of_number():
     print('3)Функция выводит модуль числа.')
-    a = input('Type a number: ')
-    print('Module of your number is: ', abs(int(a)))
+    number = input('Type a number: ')
+    return abs(int(number))
 
 
 module_of_number()
@@ -42,10 +42,10 @@ module_of_number()
 # d
 def sum_of_numbers():
     print('4)Функция выводит сумму двух чисел.')
-    a = input('Type 1st number: ')
-    b = input('Type 2nd number: ')
-    c = int(a) + int(b)
-    print('Sum of your numbers is: ', c)
+    first = input('Type 1st number: ')
+    second = input('Type 2nd number: ')
+    result = int(first) + int(second)
+    print('Sum of your numbers is: ', result)
 
 
 sum_of_numbers()
@@ -54,14 +54,13 @@ sum_of_numbers()
 # e
 def is_positive_number():
     print('5)Функция выводит информацию про знак числа.')
-    a = input('Type a number: ')
-    if int(a) > 0:
+    number = input('Type a number: ')
+    if int(number) > 0:
         print('Positive')
-    elif int(a) == 0:
+    elif int(number) == 0:
         print('Zero, neither negative nor positive')
     else:
         print('Negative')
 
 
 is_positive_number()
-
